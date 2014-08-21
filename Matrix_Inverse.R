8bca88dee0ade6e7ea22efd9142b56a909a44605
rohankudtarkar <rohan.skudtarkar@gmail.com>
2014-08-21 11:32:51 +0530

    Matrix Inverse

--- Matrix_Inverse.R
+++ Matrix_Inverse.R
@@ -1,23 +1,23 @@
 makeCacheMatrix <- function(x = matrix()) { 
-	I<-matrix()
-	set <- function(y=matrix()) {
-		x <<- y  
-		I <<- matrix()
-	}
-	get <- function() { x } 
-	setMatrix <- function(solve)
-		{ I <<- solve }
-	getMatrix<-function() I
-	list(set=set, get=get,setMatrix=setMatrix,getMatrix=getMatrix)
+    I<-matrix()
+    set <- function(y=matrix()) {
+      x <<- y  
+      I <<- matrix()
+    }
+    get <- function() { x } 
+    setMatrix <- function(solve)
+      { I <<- solve }
+    getMatrix<-function() I
+    list(set=set, get=get,setMatrix=setMatrix,getMatrix=getMatrix)
 }
 cacheSolve <- function(x=matrix(), ...) {
-    	I<-x$getMatrix()
-    	if(!is.na(I)){
-      	message("getting cached data")
-      	return(I)
-    	}
-    	tempmatrix<-x$get()
-    	I<-solve(tempmatrix,...)
-    	x$setMatrix(I)
-    	I
+  I<-x$getMatrix()
+  if(!is.na(I)){
+    message("getting cached data")
+    return(I)
+  }
+  tempmatrix<-x$get()
+  I<-solve(tempmatrix,...)
+  x$setMatrix(I)
+  I
 }
