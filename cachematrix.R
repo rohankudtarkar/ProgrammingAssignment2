a6625c119d37291dd3a0c900b599c5482b2a943e
rohankudtarkar <rohan.skudtarkar@gmail.com>
2014-08-22 10:23:25 +0530

    Update cachematrix.R
    Final Assignment Submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,26 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) {  # input x will be a matrix
+  I<-NULL                                  #I will be our Inverse Matrix and it's reset to NULL every 
+  set <- function(y=matrix()) {            # takes an input Matrix
+    x <<- y                                # saves the input Matrix
+    I <<- NULL                             # resets the inverse matrix to NULL
+  }
+  get <- function() { x }                  # this function returns the value of the original matrix
+  setMatrix <- function(solve)             # this is called by cacheSolve() during the first cacheSolve()
+  { I <<- solve }                        # access and it will store the value using superassignment
+  getMatrix<-function() I                  # this will return the cached matrix to cacheSolve() on subsequent accesses
+  
+  #  This list is returned with the newly created object.
+  
+  list(set=set, get=get,setMatrix=setMatrix,getMatrix=getMatrix)
 }
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x=matrix(), ...) {   # the input is an object created by makeCacheMatrix
+  I<-x$getMatrix()                          # accesses the object 'x' and gets the value of the Inverse Matrix
+  if(!is.null(I)){                          # if inverse was already cached (not NULL) ...
+    message("getting cached data")          # ... send this message to the console
+    return(I)                               # ... and return the inverse ... "return" ends 
+  }
+  tempmatrix<-x$get()                       # we reach this code only if x$getMatrix() returned NULL
+  I<-solve(tempmatrix,...)                  # if m was NULL then we have to calculate the Inverse
+  x$setMatrix(I)                            # store the calculated Inverse Matrix value in x 
+  I                                         # return the Inverse Matrix to the code that called this function
 }
